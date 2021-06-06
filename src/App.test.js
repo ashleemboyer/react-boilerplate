bad49112d958a86a779391c67118214411ef6a26
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2021-06-06 17:27:16 -0400

    Adding Jest and a test for App

---
+++ src/App.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import App from './App';
+
+describe('<App />', () => {
+  it('Renders without error', () => {
+    render(<App />);
+  });
+});
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -13,27 +13,6 @@ module.exports = {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/,
-        options: {
-          presets: [
-            [
-              '@babel/preset-env',
-              {
-                targets: [
-                  'last 2 versions',
-                  'not dead',
-                  'not < 2%',
-                  'not ie 11',
-                ],
-                useBuiltIns: 'entry',
-              },
-            ],
-            '@babel/preset-react',
-          ],
-          plugins: [
-            'react-hot-loader/babel',
-            '@babel/plugin-syntax-dynamic-import',
-          ],
-        },
       },
       {
         test: /\.css$/,
