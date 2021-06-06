d6321130ce232c94e6a300a715a3550d45f45c7e
Ashlee Boyer <ashleemboyer.dev@gmail.com>
2021-06-06 11:54:22 -0400

    Adding HtmlWebpackPlugin and template

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -19,4 +20,5 @@ module.exports = {
       },
     ],
   },
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
 };
